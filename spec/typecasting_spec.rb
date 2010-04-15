af8db05cc3dc6d73cec6b5a0d218eec0c65410ed
SSDany <inadsence@gmail.com>
2010-04-15 15:42:39 +0400

    just a pending

---
+++ spec/typecasting_spec.rb
@@ -0,0 +1,11 @@
+require File.expand_path File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe SMG::Resource, "typecasting" do
+
+  it "works" do
+    pending "write me, please"
+  end
+
+end
+
+#EOF
