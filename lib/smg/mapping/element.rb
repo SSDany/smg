1376658e76e86c2ec5b1bf95d72deb1d04b02583
SSDany <inadsence@gmail.com>
2010-06-05 20:54:37 +0400

    SMG::Mapping::Element#with?, just to check attributes

--- lib/smg/mapping/element.rb
+++ lib/smg/mapping/element.rb
@@ -2,13 +2,14 @@ module SMG #:nodoc:
   class Mapping #:nodoc:
 
     class Element
-      attr_reader :path, :name, :accessor, :data_class, :cast_to, :at, :context
+      attr_reader :path, :name, :accessor, :data_class, :cast_to, :at, :context, :with
 
       def initialize(path, options = {})
 
         @name       = (options[:as] || options[:at] || path.last).to_sym
         @path       = path
         @collection = !!options[:collection]
+        @with       = options[:with] ? options[:with].to_hash : nil
         @accessor   = @collection ? :"append_to_#{@name}" : :"#{@name}="
         @data_class = nil
         @cast_to    = nil
@@ -28,7 +29,7 @@ module SMG #:nodoc:
           klass = options[:class]
           if SMG::Model === klass
             @data_class = klass
-          elsif TypeCasts.key?(klass)
+          elsif SMG::Mapping::TypeCasts.key?(klass)
             @cast_to = klass
           else
             raise ArgumentError, ":class should be an SMG::Model or a valid typecast"
@@ -41,7 +42,7 @@ module SMG #:nodoc:
       end
 
       def cast(value)
-        @cast_to ? ::SMG::Mapping::TypeCasts[@cast_to, value] : value
+        @cast_to ? SMG::Mapping::TypeCasts[@cast_to, value] : value
       rescue
         raise ArgumentError, "#{value.inspect} is not a valid source for #{@cast_to.inspect}"
       end
@@ -52,6 +53,11 @@ module SMG #:nodoc:
 
       def in_context_of?(context)
         @context.nil? || @context.include?(context)
+      end
+
+      def with?(attrh)
+        @with.each { |k,v| return false unless attrh[k] == v } if @with # attrh.include?(k) && attrh[k] == v
+        true
       end
 
     end
