71f40aee35a34d7d2db14e1d7185cf4d40b43f16
SSDany <inadsence@gmail.com>
2010-06-09 20:17:42 +0400

    some specs for SMG::Model.collect

--- spec/filtering_spec.rb
+++ spec/filtering_spec.rb
@@ -31,7 +31,7 @@ describe SMG::Model, ".extract" do
 
   end
 
-  describe "using :with and :at options alltogether" do
+  describe "using :with and :at options together" do
 
     it "extracts content of the element if conditions are OK" do
       conditions = {:type => "modification", :date => "2007-10-06"}
@@ -53,7 +53,7 @@ describe SMG::Model, ".extract" do
 
   end
 
-  describe "using :with and :class options alltogether" do
+  describe "using :with and :class options together" do
 
     before :each do
       @game = Class.new { include SMG::Resource }
@@ -94,16 +94,67 @@ describe SMG::Model, ".collect" do
 
   describe "using :with option" do
 
-    it "is able to collect attributes, if conditions are OK" do
+    it "collects texts, if conditions are OK" do
+      @klass.collect 'games/game', :with => {"type" => "episodic"}, :as => :titles
+      parsed = @klass.parse(@data)
+      parsed.titles.should == ["Half-Life 2: Episode One", "Half-Life 2: Episode Two", "Half-Life 2: Episode Three"]
+    end
+
+    it "collects texts, if conditions are OK" do
+      @klass.collect 'games/game', :with => {"type" => "unknowb"}, :as => :titles
+      parsed = @klass.parse(@data)
+      parsed.titles.should be_empty
+    end
+
+  end
+
+  describe "using :with and :at options together" do
+
+    it "collects attributes, if conditions are OK" do
       @klass.collect 'games/game', :with => {"type" => "episodic"}, :at => :date, :as => :dates
       parsed = @klass.parse(@data)
       parsed.dates.should == ["2006-06-01", "2007-10-10", "To be announced"]
     end
 
-    it "is able to collect texts, if conditions are OK" do
-      @klass.collect 'games/game', :with => {"type" => "episodic"}, :as => :titles
+    it "collects nothing otherwise" do
+      @klass.collect 'games/game', :with => {"type" => "unknown"}, :at => :date, :as => :dates
       parsed = @klass.parse(@data)
-      parsed.titles.should == ["Half-Life 2: Episode One", "Half-Life 2: Episode Two", "Half-Life 2: Episode Three"]
+      parsed.dates.should be_empty
+    end
+
+  end
+
+  describe "using :with and :class options together" do
+
+    before :each do
+      @game = Class.new { include SMG::Resource }
+      @game.extract :game, :at => "AppID", :as => :steam_application_id, :class => :integer
+      @game.extract :game, :at => "date"
+      @game.extract :game, :as => :title
+    end
+
+    it "extracts content of the element if conditions are OK" do
+      conditions = {:type => "modification"}
+      @klass.collect 'games/game', :class => @game, :with => conditions, :as => :games
+      collection = @klass.parse(@data)
+
+      collection.games.should_not be_empty
+      collection.games.size.should == 2
+
+      collection.games[0].title.should == "Minerva: Metastasis"
+      collection.games[0].steam_application_id.should == 380
+      collection.games[0].date.should == "2007-10-06"
+
+      collection.games[1].title.should == "Black Mesa"
+      collection.games[1].steam_application_id.should == nil
+      collection.games[1].date.should == "To be announced"
+    end
+
+    it "extracts nothing otherwise" do
+      conditions = {:type => "unknown"}
+      @klass.collect 'games/game', :class => @game, :with => conditions, :as => :games
+      collection = @klass.parse(@data)
+      collection.games.should be_empty
     end
 
   end
